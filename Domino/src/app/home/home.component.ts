a8ab5e82621071ce44b5fa2d9b0f8f86d51ee157
nkhaclap <nkhaclap@vnpt.vn>
2023-10-17 15:11:24 +0700

    d

--- Domino/src/app/home/home.component.ts
+++ Domino/src/app/home/home.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
+  public name = 'Kuzinkiller';
+  constructor() {}
 }
